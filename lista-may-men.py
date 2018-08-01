e384a1273338f59cd6b85d6e7b1d6427a9e8c412
matiasfare <matiasfare59@gmail.com>
2018-08-01 16:41:24 -0400

    Lee dos numero e imprime el mayor

--- lista-may-men.py
+++ lista-may-men.py
@@ -1,2 +1,12 @@
-!bin
-num=
+#!/usr/bin/python3
+
+num1 = int(input("Escriba un numero positivo: "))
+num2 = int(input("Escriba otro numero positivo: "))
+
+if num1 < num2:
+   maxi = num2 
+else:
+   maxi = num1 
+   
+print("El numero mayor es:", maxi)
+
