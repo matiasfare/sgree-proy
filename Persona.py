57bc1343b5ac12c83c15538f50db9c80751e96cc
matiasfare <matiasfare59@gmail.com>
2018-10-22 23:59:45 -0300

    Committ inicial del proyecto-SGREE

---
+++ Persona.py
@@ -0,0 +1,8 @@
+
+class Persona():
+    '''Clase Base para todas las personas'''
+
+    def __init__ (self, nombre, apellido, cedula):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.cedula = cedula
---
+++ Recibo.py
@@ -0,0 +1,11 @@
+#almacena cualquier ficha que se le ingrese
+from myzodb.py import MiDataBase, transaction
+
+fb = MiDataBase('Data.fs')
+dbroot = db.raiz
+
+
+class Recibo(Ficha):
+
+    num_rec = 0
+    def __init__ (self, )
