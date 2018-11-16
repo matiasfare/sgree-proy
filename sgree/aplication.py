1b91cfa19a0229f54b3fe8dd785c37baf6802dd4
matiasfare <matiasfare59@gmail.com>
2018-11-16 14:45:25 -0300

    Define llamada a interfaz GUI

--- sgree/aplication.py
+++ sgree/aplication.py
@@ -1,14 +1,17 @@
-# from sistema.Empresa import *
-import sys
-import os
-# ruta = os.getcwd()
-# sys.path.insert(0, ruta+'sgree')
-from controller import *
-from empresa import *
+import tkinter
+import view
+
+root = tkinter.Tk()
+vista = view.vista_principal()
+view = view.add_recibo()
+
+root.mainloop()
 
 
-c = Controller()
-c.crear_recibo()
+
+
+
+#c.crear_recibo()
 # c.agregar_cliente()
 # c.listar_clientes()
 # c.imprimir_recibo()
