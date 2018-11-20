4649bbf8f308a2778d21f7b835021849698ac8b6
matiasfare <matiasfare59@gmail.com>
2018-11-19 22:50:14 -0300

    Controla intereaccion into view and controller

--- sgree/controller.py
+++ sgree/controller.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
 #-----------------------------------CONTROLLER-------------------------------------------------
-from model import Model
-from view import View
+import model as Model
+import view as View
 from persistent import Persistent
 
-class Controller(Persistent):
+class Controller(Persistent): 
         def __init__(self):
             '''Clase controlador De: Modelo y Vista'''
-            self.model = Model()
-            self.view = View()
+            self.model = Model.Model()
+            self.view = View.View()
+            
+            
 
-        def crear_recibo(self):
-            '''Controlador que se comunica con la vista y el modelo para Agregar o Crear Recibo'''
-            new_recibo = self.view.vista_crear_recibo()
-            recibo = self.model.persistir_objeto(new_recibo,'recibo')
-            self.view.vista_imprimir_recibo_guardado(recibo)
+        # def crear_recibo(self):
+        #     '''Controlador que se comunica con la vista y el modelo para Agregar o Crear Recibo'''
+        #     new_recibo = self.view.vista_crear_recibo()
+        #     recibo = self.model.persistir_objeto(new_recibo,'recibo')
+        #     self.vista_imprimir_recibo_guardado(recibo)
 
-        def agregar_cliente(self):
+        def controll_add_cliente(self):
             '''Controlador que llama al modelo y la vista para agregar un Cliente'''
-            cliente = self.view.vista_agregar_cliente()
-            self.model.persistir_objeto(cliente,'cliente')
+            cliente = self.view.add_cliente()
+            obj = self.model.crear(cliente,'Clientes',cliente.documento)
+            print (obj.apellido, obj.nombre, obj.documento, obj.contacto)
 
-        def listar_clientes(self):
-            '''Controlador que se comunica con la vista y el modelo para listar Clientes'''
-            lista_de_clientes = self.model.listar('cliente')
-            self.view.vista_listar_clientes(lista_de_clientes)
-            return lista_de_clientes
+        # def listar_clientes(self):
+        #     '''Controlador que se comunica con la vista y el modelo para listar Clientes'''
+        #     lista = self.model.obtener_lista('Clientes')
+        #     self.view.vista_listar(lista)
 
-        def buscar_por_cedula(self):
-            '''Controlador que se comunica con la vista y el modelo para Buscar Cliente por Cedula'''
-            cedula = self.view.vista_buscar_por_cedula()
-            respuesta = self.model.buscar_por_cedula(cedula)
-            self.view.vista_imprimir_persona_buscada_por_cedula(respuesta)
+        # def buscar_por_cedula(self):
+        #     '''Controlador que se comunica con la vista y el modelo para Buscar Cliente por Cedula'''
+        #     cedula = self.view.vista_buscar_por_cedula()
+        #     respuesta = self.model.buscar_por_cedula(cedula)
+        #     self.view.vista_imprimir_persona_buscada_por_cedula(respuesta)
 
-        def listar_recibos(self):
-            '''Imprime Solo Recibos existentes'''
-            lista_recibos = self.model.listar_recibos('recibo')
-            self.view.vista_listar_recibos(lista_recibos)
-            return lista_recibos
+        # def listar_recibos(self):
+        #     '''Imprime Solo Recibos existentes'''
+        #     lista_recibos = self.model.listar_recibos('recibo')
+        #     self.vista_listar_recibos(lista_recibos)
+        #     return lista_recibos
 
-        def editar_recibo(self):
-            '''Permitira editar un recibo'''
-        pass
+
