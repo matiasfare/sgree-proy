2c47df2aa5c9d9b415ed254ed43bb4ce02cc5d59
matiasfare <matiasfare59@gmail.com>
2018-11-26 21:59:07 -0300

    Se definene los primeros hechos prolog

---
+++ prolog/prolo.pl
@@ -0,0 +1,18 @@
+es_un_cliente("123").
+
+es_cedula_de_cliente("5719493","123").
+
+es_nombre_de_cliente("Matias","123").
+
+es_apellido_de_cliente("Fare","123").
+
+es_fecha_de_recibo("2018/11/23","123").
+
+es_observacion_de_recibo("no prende","123").
+
+es_presupuesto_de_recibo("120.000","123").
+
+es_validez_de_recibo("15 Dias","123").
+
+
+son_datos_de_cliente()
