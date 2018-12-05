e0ea0a5ca60699fa2d4473d8179a1bf49ba8381e
matiasfare <matiasfare59@gmail.com>
2018-12-05 09:58:03 -0300

    Refina Script Prolog

--- prolog/prolo.pl
+++ prolog/prolo.pl
@@ -1,6 +1,8 @@
-es_un_cliente("123").
+%%% Hechos de los Recibos
 
-es_cedula_de_cliente("5719493","123").
+es_un_recibo("123").
+
+es_cliente_de_recibo("5719493","123").
 
 es_nombre_de_cliente("Matias","123").
 
@@ -15,4 +17,38 @@ es_presupuesto_de_recibo("120.000","123").
 es_validez_de_recibo("15 Dias","123").
 
 
-son_datos_de_cliente()
+%%% HECHOS DE UN CLIENTE
+
+es_cliente("5719493").
+
+es_nombre_de_cliente("Matias","5719493").
+es_apellido_de_cliente("Fare","5719493").
+es_contacto_de_cliente("0984908298","5719493")
+
+%%% REGLA DE RECIBO
+
+son_atributos_de_Recibo(Cedula, Nombre, Apellido,Observacion,Presupuesto,Validez):-
+es_cliente_de_recibo(Cedula,Recibo),
+es_fecha_de_recibo(Fecha,Recibo),
+es_observacion_de_recibo(Observacion, Recibo),
+es_presupuesto_de_recibo(Presupuesto, Recibo),
+es_validez_de_recibo(Validez,Recibo).
+
+# % HECHOS:
+# es_un_pais(paraguay).
+# es_un_pais(brasil).
+# es_un_pais(argentina).
+# es_un_pais(uruguay).
+# es_un_pais(bolivia).
+# limita_con(paraguay, argentina).
+# limita_con(paraguay, brasil).
+# limita_con(paraguay, bolivia).
+# superficie_pais(paraguay, 406752).
+# superficie_pais(brasil, 8547404).
+
+# % REGLAS:
+# limitrofes(X,Y):- limita_con(X,Y);
+
+# %OR
+# limita_con(Y,X).
+# pais_pequeño(X) :- superficie_pai
